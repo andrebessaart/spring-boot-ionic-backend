50e390f8674de2fad206814cc9b94d73ecd6a44e
Bessa <andrebessaart@gmail.com>
2018-09-08 13:05:39 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/br/com/andrebessaart/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/br/com/andrebessaart/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import br.com.andrebessaart.cursomc.services.exceptions.DateIntegrityException;
 import br.com.andrebessaart.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -17,5 +18,12 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DateIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DateIntegrityException e, HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
--- src/main/java/br/com/andrebessaart/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/andrebessaart/cursomc/services/CategoriaService.java
@@ -2,11 +2,15 @@ package br.com.andrebessaart.cursomc.services;
 
 import java.util.Optional;
 
+import javax.validation.ConstraintViolationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.andrebessaart.cursomc.domain.Categoria;
 import br.com.andrebessaart.cursomc.repositories.CategoriaRepository;
+import br.com.andrebessaart.cursomc.services.exceptions.DateIntegrityException;
 import br.com.andrebessaart.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -28,5 +32,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+	   find(id);
+	   try {
+		   repo.deleteById(id);
+	   }catch(DataIntegrityViolationException e) {
+		   throw new DateIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+	   }
+	   
 	}
 }
