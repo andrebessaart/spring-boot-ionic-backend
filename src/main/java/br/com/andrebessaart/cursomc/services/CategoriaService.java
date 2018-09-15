c3389b991233154d62f881289e2299c218277446
Bessa <andrebessaart@gmail.com>
2018-09-15 11:54:32 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/br/com/andrebessaart/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/andrebessaart/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.andrebessaart.cursomc.domain.Categoria;
+import br.com.andrebessaart.cursomc.dto.CategoriaDTO;
 import br.com.andrebessaart.cursomc.repositories.CategoriaRepository;
 import br.com.andrebessaart.cursomc.services.exceptions.DateIntegrityException;
 import br.com.andrebessaart.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
